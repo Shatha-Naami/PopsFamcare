61b3211124b50535d37abadfc536cc8e36a91492
Shatha-Naami <shathaahmad512@gmail.com>
2021-03-17 15:50:54 +0200

    convert screen to dialog..

--- lib/PopsComponents/CancelMeetingWidget.dart
+++ lib/PopsComponents/CancelMeetingWidget.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'ButtonComponent.dart';
 import 'MessagesComponents.dart';
 
+
 class CancelMeetingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
